6c6f2e7754f4aeb9313663a7e6f255838d816901
xyz <xyzhtml@163.com>
2015-07-23 14:29:33 +0800

    update to fit for lark-0-11-0

--- controllers/demo/index.js
+++ controllers/demo/index.js
@@ -1,8 +1,23 @@
+var Router = require('lark/node_modules/lark-router/node_modules/koa-router');
+
 module.exports = function(router){
-    router.get('/', function *(next){
-        this.body = '111' + this.pageServices.demo.render() 
+    var demo = function* (next){
+        //this.body = '111' + this.pageServices.demo.render() 
+        this.body = ' demo index'
         //this.body = "demo<br><a href='/demo/list/123'>list123</a><br><a href='/demo/article/123'>article123</a>"
         yield next;
-    });
+    }
+
+    router.get('/', demo);
+
+    ///////////////////////
+
+    var comp_router = new Router();
+        comp_router.get('/test', demo);
+
+    this.use(comp_router.routes());
+
+    ///////////////////////
+
     return router;
 };
--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,9 @@ module.exports = function(router){
     router.get('/', function*(next){
         this.body = 'hello index'
         yield next
+    }).get('/:bar*', function * testp1(next){
+        this.body = this.params.bar
+        yield next
     });
     return router
 }
